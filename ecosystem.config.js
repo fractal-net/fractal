ea93004c8e64506930d3af352fa19bb8a2b53908
theAdoringFan <157860668+theAdoringFan@users.noreply.github.com>
2024-02-16 14:40:15 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,13 +15,13 @@ module.exports = {
             name: 'neurons-prover',
             script: 'python',
             interpreter: 'none',
-            args: 'neurons/prover/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:<SERVER_PORT_HERE>" --logging.debug --logging.trace --netuid 5 --axon.port <YOUR_INTERNAL_PORT> --axon.external_port <YOUR_EXTERNAL_PORT> --wallet.name <YOUR_WALLET_NAME> --wallet.hotkey <YOUR_WALLET_HOTKEY>'
+            args: 'neurons/prover/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:<SERVER_PORT_HERE>" --logging.debug --logging.trace --netuid 29 --axon.port <YOUR_INTERNAL_PORT> --axon.external_port <YOUR_EXTERNAL_PORT> --wallet.name <YOUR_WALLET_NAME> --wallet.hotkey <YOUR_WALLET_HOTKEY>'
         },
         // {
         //     name: 'neurons-verifier',
         //         script: 'python',
         //         interpreter: 'none',
-        //         args: 'neurons/verifier/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:<SERVER_PORT_HERE>" --logging.debug --logging.trace --database.password <YOUR_PASSWORD_HERE> --netuid 5 --axon.port <YOUR_INTERNAL_PORT> --axon.external_port <YOUR_EXTERNAL_PORT> --wallet.name <YOUR_WALLET_NAME> --wallet.hotkey <YOUR_WALLET_HOTKEY>''
+        //         args: 'neurons/verifier/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:<SERVER_PORT_HERE>" --logging.debug --logging.trace --database.password <YOUR_PASSWORD_HERE> --netuid 29 --axon.port <YOUR_INTERNAL_PORT> --axon.external_port <YOUR_EXTERNAL_PORT> --wallet.name <YOUR_WALLET_NAME> --wallet.hotkey <YOUR_WALLET_HOTKEY>''
         // }
     ]
 };

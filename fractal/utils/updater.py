985f03a9c6064eb9bf80835516b6e56255d326f7
girlfriend-deficit <girlfrienddeficit@skiff.com>
2024-02-26 22:40:05 -0500

    save

--- fractal/utils/updater.py
+++ fractal/utils/updater.py
@@ -24,14 +24,11 @@ import fractal
 
 def autoupdate(branch: str = "main"):
     '''
-    Automatically updates the Targon codebase to the latest version available on the specified branch.
+    Automatically updates the Targon codebase to the latest version available.
 
-    This function checks the remote repository for the latest version of Targon by fetching the VERSION file from the specified branch.
+    This function checks the remote repository for the latest version of Fractal by fetching the VERSION file from the main branch.
     If the local version is older than the remote version, it performs a git pull to update the local codebase to the latest version.
     After successfully updating, it restarts the application with the updated code.
-
-    Parameters:
-    - branch (str): The name of the branch to check for updates. Defaults to "main".
 
     Note:
     - The function assumes that the local codebase is a git repository and has the same structure as the remote repository.
@@ -41,35 +38,37 @@ def autoupdate(branch: str = "main"):
     '''
     bt.logging.info("Checking for updates...")
     try:
+        # Fetching latest version from the main branch
         response = requests.get(
             f"https://raw.githubusercontent.com/fractal-net/fractal/{branch}/VERSION",
             headers={'Cache-Control': 'no-cache'}
         )
         response.raise_for_status()
-        repo_version = response.content.decode()
-        latest_version = [int(v) for v in repo_version.split(".")]
-        local_version = [int(v) for v in fractal.__version__.split(".")]
+        latest_version = response.content.decode()
+        
+        latest_version_tuple = tuple(map(int, latest_version.split(".")))
+        local_version_tuple = tuple(map(int, fractal.__version__.split(".")))
 
-        bt.logging.info(f"Local version: {fractal.__version__}")
-        bt.logging.info(f"Latest version: {repo_version}")
-
-        if latest_version > local_version:
+        # Checking if local version is older than the remote version
+        if latest_version_tuple > local_version_tuple:
             bt.logging.info("A newer version of Fractal is available. Updating...")
             base_path = os.path.abspath(__file__)
             while os.path.basename(base_path) != "fractal":
                 base_path = os.path.dirname(base_path)
             base_path = os.path.dirname(base_path)
 
+            # Performing git pull to update to the latest version
             os.system(f"cd {base_path} && git pull")
 
+            # Checking the VERSION file in the main branch again after the update
             with open(os.path.join(base_path, "VERSION")) as f:
                 new_version = f.read().strip()
-                new_version = [int(v) for v in new_version.split(".")]
 
-                if new_version == latest_version:
-                    bt.logging.info("Fractal updated successfully. Restarting...")
-                    os.execv(sys.executable, [sys.executable] + sys.argv)
-                else:
-                    bt.logging.error("Update failed. Manual update required.")
+            # Restarting the application if the update was successful
+            if new_version == latest_version:
+                bt.logging.info("Fractal updated successfully. Restarting...")
+                os.execv(sys.executable, [sys.executable] + sys.argv)
+            else:
+                bt.logging.error("Update failed. Manual update required.")
     except Exception as e:
         bt.logging.error(f"Update check failed: {e}")

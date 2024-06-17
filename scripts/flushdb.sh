cf526e37bc8796ea3dbe586ac0a9188c521c83e2
girlfriend-deficit <girlfrienddeficit@skiff.com>
2024-06-17 11:05:24 -0400

    flushall

--- scripts/flushdb.sh
+++ scripts/flushdb.sh
@@ -1,15 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 # The host of the Redis server
 REDIS_HOST="localhost"
 # The port the Redis server is running on
 REDIS_PORT=6379
-# The Redis database number to clear
-REDIS_DB=0
 # Redis password 
 REDIS_PASSWORD="your_password_here"
 
-# Connect to the Redis server and execute FLUSHDB
-redis-cli -h $REDIS_HOST -p $REDIS_PORT -n $REDIS_DB -a $REDIS_PASSWORD FLUSHDB
-
+# Connect to the Redis server and execute FLUSHALL
+redis-cli -h $REDIS_HOST -p $REDIS_PORT -a $REDIS_CS_PASSWORD FLUSHALL
 
